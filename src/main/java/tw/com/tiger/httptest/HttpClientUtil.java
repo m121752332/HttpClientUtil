3147e043d4d24a17a3c50a87dc4be91b629e0cc7
Game <m121752332@gmail.com>
2020-10-14 22:08:56 +0800

    專案上傳

---
+++ src/main/java/tw/com/tiger/httptest/HttpClientUtil.java
@@ -0,0 +1,44 @@
+package tw.com.tiger.httptest;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.util.EntityUtils;
+public class HttpClientUtil {
+
+    public static void main(String[] args){
+        String url = "https://hooks.slack.com/services/AAAA/BBBB";
+        String jsonStr = "{\"text\":\"java test\"}";
+        String httpOrgCreateTestRtn = HttpClientUtil.doPost(url, jsonStr, "utf-8");
+        System.out.println(httpOrgCreateTestRtn);
+    }
+
+    @SuppressWarnings("resource")
+    public static String doPost(String url,String jsonstr,String charset){
+        HttpClient httpClient = null;
+        HttpPost httpPost = null;
+        String result = null;
+        try{
+            httpClient = new SSLClient();
+            httpPost = new HttpPost(url);
+            httpPost.addHeader("Content-Type", "application/json");
+            StringEntity se = new StringEntity(jsonstr);
+            se.setContentType("text/json");
+            se.setContentEncoding(new BasicHeader("Content-Type", "application/json"));
+            httpPost.setEntity(se);
+            HttpResponse response = httpClient.execute(httpPost);
+            if(response != null){
+                HttpEntity resEntity = response.getEntity();
+                if(resEntity != null){
+                    result = EntityUtils.toString(resEntity,charset);
+                }
+            }
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return result;
+    }
+}
